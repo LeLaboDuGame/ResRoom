26cea7ce2746b49beae20c69f9af0a5e04956dd6
labo <ad2015adrien@gmail.com>
2026-06-04 20:36:46 +0200

    chore(api): add project structure and test config (refs #1975)

--- roomio-frontend/src/App.jsx
+++ roomio-frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-import RoomsList from "./pages/RoomsList";
-import RoomPage from "./pages/RoomPage";
+import Dashboard from "./pages/Dashboard";
+import FleetRoom from "./pages/FleetRoom";
+import Admin from "./pages/Admin";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<RoomsList />} />
-      <Route path="/rooms/:roomName" element={<RoomPage />} />
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/fleet/:roomName" element={<FleetRoom />} />
+      <Route path="/admin" element={<Admin />} />
     </Routes>
   );
 }
